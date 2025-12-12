49daf7407830fb347699909da29239254b6987df
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2025-12-12 17:36:00 +0000

    test fixed

--- tests/scripts/test_t1_t2_spiral_cmrf.py
+++ tests/scripts/test_t1_t2_spiral_cmrf.py
@@ -3,7 +3,7 @@
 import pytest
 from mrseq.scripts.t1_t2_spiral_cmrf import main as create_seq
 
-EXPECTED_DUR = 13.05348  # defined 2025-11-13
+EXPECTED_DUR = 14.53304  # defined 2025-12-12
 
 
 def test_default_seq_duration(system_defaults):
@@ -16,4 +16,4 @@ def test_default_seq_duration(system_defaults):
 def test_seq_creation_error_on_short_tr(system_defaults):
     """Test if error is raised on too short repetition time."""
     with pytest.raises(ValueError):
-        create_seq(system=system_defaults, tr=5e-3, show_plots=False)
+        create_seq(system=system_defaults, tr=3e-3, show_plots=False)
