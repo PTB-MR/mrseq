e50a366dad901328fefc66c4bcd9e10b471c0416
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2026-08-09 17:25:36 +0100

    ensure rounding

--- src/mrseq/utils/EpiReadout.py
+++ src/mrseq/utils/EpiReadout.py
@@ -210,7 +210,7 @@ class EpiReadout:
         self.spoiling_enable = spoiling_enable
 
         # Derived parameters
-        readout_time = n_readout / bandwidth
+        readout_time = round_to_raster(n_readout / bandwidth, system.grad_raster_time, method='ceil')
         delta_kx = 1 / (fov * oversampling)
         delta_ky = 1 / fov
 
@@ -222,7 +222,9 @@ class EpiReadout:
         self.gz_spoil = None
 
         # Create blip gradient with shortest possible timing
-        gy_blip_duration = np.ceil(2 * np.sqrt(delta_ky / system.max_slew) / 10e-6 / 2) * 10e-6 * 2
+        gy_blip_duration = round_to_raster(
+            2 * np.sqrt(delta_ky / system.max_slew), 2 * system.grad_raster_time, method='ceil'
+        )
         gy_blip_half_dur = gy_blip_duration / 2
         self.gy_blip = pp.make_trapezoid(channel='y', system=self.system, area=delta_ky, duration=gy_blip_duration)
 
