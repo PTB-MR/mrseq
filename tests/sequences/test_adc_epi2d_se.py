e53421a629ea39a94ba47166e97fd3e16549430f
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2026-08-14 16:43:09 +0200

    diffusion sequence with epi readout
    Co-authored-by: Helge Herthum <h.herthum@gmail.com>

---
+++ tests/sequences/test_adc_epi2d_se.py
@@ -0,0 +1,13 @@
+"""Tests for 2D EPI SE for ADC mapping."""
+
+import pytest
+from mrseq.sequences.adc_epi2d_se import main as create_seq
+
+EXPECTED_DUR = 1.38768  # defined 2026-03-18
+
+
+def test_default_seq_duration(system_defaults):
+    """Test if default values result in expected sequence duration."""
+    seq, _ = create_seq(system=system_defaults, show_plots=False)
+    duration = seq.duration()[0]
+    assert duration == pytest.approx(EXPECTED_DUR)
