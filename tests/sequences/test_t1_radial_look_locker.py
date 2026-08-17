99cd2f0f6d69f6420b240a9bff979efb9a25eedd
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2026-08-17 18:00:47 +0200

    add t1 radial look locker sequence

---
+++ tests/sequences/test_t1_radial_look_locker.py
@@ -0,0 +1,37 @@
+"""Tests for radial T1 Look Locker sequence."""
+
+import pytest
+from mrseq.sequences.t1_radial_look_locker import main as create_seq
+
+EXPECTED_DUR = 2.14728  # defined 2026-03-03
+
+
+def test_default_seq_duration(system_defaults):
+    """Test if default values result in expected sequence duration."""
+    seq, _ = create_seq(system=system_defaults, show_plots=False)
+    duration = seq.duration()[0]
+    assert duration == pytest.approx(EXPECTED_DUR)
+
+
+def test_seq_creation_error_on_short_te(system_defaults):
+    """Test if error is raised on too short echo time."""
+    with pytest.raises(ValueError):
+        create_seq(system=system_defaults, te=1e-3, show_plots=False)
+
+
+def test_seq_creation_error_on_short_tr(system_defaults):
+    """Test if error is raised on too short repetition time."""
+    with pytest.raises(ValueError):
+        create_seq(system=system_defaults, tr=2e-3, show_plots=False)
+
+
+def test_seq_predefined_echo_time(system_defaults):
+    """Test sequence with predefined echo time."""
+    seq, _ = create_seq(
+        system=system_defaults,
+        te=3e-3,
+        show_plots=False,
+        test_report=False,
+        timing_check=False,
+    )
+    assert seq
