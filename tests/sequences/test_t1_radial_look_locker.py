dd1d6dd93a4a48f0c629f7468bb52cfbcdf4f7ca
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2026-09-03 17:29:35 +0200

    adapt seq time

--- tests/sequences/test_t1_radial_look_locker.py
+++ tests/sequences/test_t1_radial_look_locker.py
@@ -3,7 +3,7 @@
 import pytest
 from mrseq.sequences.t1_radial_look_locker import main as create_seq
 
-EXPECTED_DUR = 2.14728  # defined 2026-03-03
+EXPECTED_DUR = 2.04728  # defined 2026-09-03
 
 
 def test_default_seq_duration(system_defaults):
